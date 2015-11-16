78bb1392bfab025454c8042dfce381d966575cbc
Nicolas BONNIOT <nicolas@devgiants.fr>
2015-11-16 08:47:36 +0100

    - Fos User configuration OK - Firewalls configuration OK (all under authentification, login anonymous)

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 12/11/15
+ * Time: 09:16
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
